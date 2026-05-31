754598d252648e96738c8ae11a8f0713d91770a9
tellentsynq-debug <tellentsynq@gmail.com>
2026-05-31 18:00:36 +0530

    Add company domain email validation for signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,6 +13,13 @@ exports.signup = async (req, res) => {
       });
     }
 
+    // Check if email is from company domain
+    if (!email.endsWith('@jbrstaffingsolutions.com')) {
+      return res.status(400).json({ 
+        error: 'Only @jbrstaffingsolutions.com email addresses are allowed' 
+      });
+    }
+
     if (password.length < 6) {
       return res.status(400).json({ 
         error: 'Password must be at least 6 characters long' 
