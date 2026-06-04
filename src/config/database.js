c98a445fc43b60cf4dbefba026eff0559b31fe01
tellentsynq-debug <tellentsynq@gmail.com>
2026-06-04 17:59:19 +0530

    fix the bug

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,14 @@
 const { createClient } = require('@supabase/supabase-js');
+const ws = require('ws');
 require('dotenv').config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    transport: ws
+  }
+});
 
 module.exports = supabase;
